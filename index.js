1ac2f5bcd0515986f15afe8b1394a99a7d0dea29
Matthew <matthew@stateofmind.agency>
2020-01-11 14:45:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+var num1 = 10;
+var num2 = 6.2;
+var multiply = num1 * num2;
+
+var random = Math.ceil(Math.random() * 10) + 1;
+
+var max = Math.max(10, 20, 5)
+
+var mod = 20 % 8;
